d04475b3854cb38d97cd3ba02a44841dd4a95f86
Matyáš Herman <matysek.herman@gmail.com>
2021-05-27 13:38:30 +0200

    BlogPost styled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="home">
-    
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost v-for="(post, index) in sampleBlogPost" :key="index" :post="post"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost';
+
 export default {
-  name: "Home",
-  components: {},
+  name: 'Home',
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, Javascript and more.',
+        welcomeScreen: true,
+        photo: 'coding',
+      },
+      sampleBlogPost: [
+        {
+          title: 'This is a Filter Title!',
+          blogHTML: 'This is a filter blog post title',
+          blogCoverPhoto: 'beautiful-stories',
+        },
+        {
+          title: 'This is a Filter 2 Title!',
+          blogHTML: 'This is a filter blog post title',
+          blogCoverPhoto: 'designed-for-everyone',
+        },
+      ],
+    };
+  },
 };
 </script>

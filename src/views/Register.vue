b2684472c25a96cd3a5d2c5a43ffe48be6c0978d
Matyáš Herman <matysek.herman@gmail.com>
2021-05-28 15:39:20 +0200

    registration finished

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -29,8 +29,9 @@
           <input type="password" placeholder="Password" v-model="password" />
           <password class="icon" />
         </div>
+        <div v-show="error" class="error">{{ this.errorMsg }}</div>
       </div>
-      <button>Sign Up</button>
+      <button @click.prevent="register">Sign Up</button>
       <div class="angle"></div>
     </form>
     <div class="background"></div>
@@ -41,6 +42,9 @@
 import email from '../assets/Icons/envelope-regular.svg';
 import password from '../assets/Icons/lock-alt-solid.svg';
 import user from '../assets/Icons/user-alt-light.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
 
 export default {
   name: 'Register',
@@ -50,21 +54,56 @@ export default {
     user,
   },
   data() {
-      return {
-          firstName: null,
-          lastName: null,
-          username: null,
-          email: null,
-          password: null,
+    return {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      error: null,
+      errorMsg: '',
+    };
+  },
+  methods: {
+    async register() {
+      if (
+        this.email !== '' &&
+        this.password !== '' &&
+        this.firstName !== '' &&
+        this.lastName !== '' &&
+        this.username !== ''
+      ) {
+        this.error = false;
+        this.errorMsg = '';
+        const firebaseAuth = await firebase.auth();
+        const createUser = await firebaseAuth.createUserWithEmailAndPassword(
+          this.email,
+          this.password
+        );
+        const result = await createUser;
+        const dataBase = db.collection('users').doc(result.user.uid);
+        await dataBase.set({
+          firstName: this.firstName,
+          lastName: this.lastName,
+          username: this.username,
+          email: this.email,
+        });
+        this.$router.push({ name: 'Home' });
+        return;
       }
-  }
+
+      this.error = true;
+      this.errorMsg = 'Please fill out all the fields!';
+      return;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .register {
-    h2 {
-        max-width: 350px;
-    }    
+  h2 {
+    max-width: 350px;
+  }
 }
 </style>

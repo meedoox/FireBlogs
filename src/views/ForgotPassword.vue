3bf2b4d740b749a5b85acaaffbeffb71b82251eb
Matyáš Herman <matysek.herman@gmail.com>
2021-05-28 11:43:37 +0200

    Forgot Password - HTML structure

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,29 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+      <div class="form-wrap">
+          <form class="reset">
+              <h2>Reset Password</h2>
+              <p>Forgot your password? Enter your email to reset it</p>
+              <div class="inputs">
+                  <div class="input">
+                      <input type="text" placeholder="Email" v-model="email" />
+                      <email class="icon" />
+                  </div>
+              </div>
+              <button>Reset</button>
+              <div class="angle"></div>
+          </form>
+          <div class="background"></div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    name: "ForgotPassword",
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>

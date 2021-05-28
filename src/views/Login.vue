eac6b07d910994a2a4ba8717b34c7bd59bf314f4
Matyáš Herman <matysek.herman@gmail.com>
2021-05-28 10:29:03 +0200

    Login - CSS login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -47,4 +47,97 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+.form-wrap {
+    overflow: hidden;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-self: center;
+    margin: 0 auto;
+    width: 90%;
+
+    @media (min-width: 900px) {
+        width: 100%;
+    }
+
+    .login-register {
+        margin-bottom: 32px;
+
+        .router-link {
+            color: #000;
+        }
+    }
+    
+    form {
+        padding: 0 10px;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+
+        @media (min-width: 900px) {
+            padding: 0 50px;
+        }
+
+        h2 {
+            text-align: center;
+            font-size: 32px;
+            color: #303030;
+            margin-bottom: 40px;
+
+            @media (min-width: 900px) {
+                font-size: 40px;
+            }
+        }
+
+        .inputs {
+            width: 100%;
+            max-width: 350px;
+
+            .input {
+                position: relative;
+                display:flex;
+                justify-content: center;
+                align-items: center;
+                margin-bottom: 8px;
+
+                input {
+                    width: 100%;
+                    border: none;
+                    background-color: #f2f7f6;
+                    padding: 4px 4px 4px 30px;
+                    height: 50px;
+
+                    &:focus {
+                        outline: none;
+                    }
+                }
+
+                .icon {
+                    width: 12px;
+                    position: absolute;
+                    left: 6px;
+                }
+            }
+        }
+
+        .forgot-password {
+            text-decoration: none;
+            color: #000;
+            cursor: pointer;
+            font-size: 14px;
+            margin: 16px 0 32px;
+            border-bottom: 1px solid transparent;
+            transition: .5s ease all;
+
+            &:hover {
+                border-color: #303030;
+            }
+        }
+    }
+}
+</style>

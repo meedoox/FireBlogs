247b589082c8660aceaf0baef8bff72a1d8da9af
Matyáš Herman <matysek.herman@gmail.com>
2021-05-28 16:38:01 +0200

    login on created

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,13 @@ export default {
     };
   },
   created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.$store.commit("updateUser", user);
+      if (user) {
+        this.$store.dispatch("getCurrentUser");
+        console.log(this.$store.state.profileEmail)
+      }
+    })
     this.checkRoute();
   },
   mounted() {},

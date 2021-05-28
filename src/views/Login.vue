2a4e72af7cde5d6b2700c777cf25f46cbadcd849
Matyáš Herman <matysek.herman@gmail.com>
2021-05-28 11:38:33 +0200

    Register page finished

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -39,133 +39,132 @@ export default {
     password,
   },
   data() {
-      return {
-          email: null,
-          password: null,
-      }
-  }
+    return {
+      email: null,
+      password: null,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-
 .form-wrap {
-    overflow: hidden;
+  overflow: hidden;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-self: center;
+  margin: 0 auto;
+  width: 90%;
+
+  @media (min-width: 900px) {
+    width: 100%;
+  }
+
+  .login-register {
+    margin-bottom: 32px;
+
+    .router-link {
+      color: #000;
+    }
+  }
+
+  form {
+    padding: 0 10px;
+    position: relative;
     display: flex;
-    height: 100vh;
+    flex-direction: column;
     justify-content: center;
-    align-self: center;
-    margin: 0 auto;
-    width: 90%;
+    align-items: center;
+    flex: 1;
 
     @media (min-width: 900px) {
-        width: 100%;
+      padding: 0 50px;
     }
 
-    .login-register {
-        margin-bottom: 32px;
+    h2 {
+      text-align: center;
+      font-size: 32px;
+      color: #303030;
+      margin-bottom: 40px;
 
-        .router-link {
-            color: #000;
-        }
+      @media (min-width: 900px) {
+        font-size: 40px;
+      }
     }
-    
-    form {
-        padding: 0 10px;
+
+    .inputs {
+      width: 100%;
+      max-width: 350px;
+
+      .input {
         position: relative;
         display: flex;
-        flex-direction: column;
         justify-content: center;
         align-items: center;
-        flex: 1;
+        margin-bottom: 8px;
 
-        @media (min-width: 900px) {
-            padding: 0 50px;
+        input {
+          width: 100%;
+          border: none;
+          background-color: #f2f7f6;
+          padding: 4px 4px 4px 40px;
+          height: 50px;
+          border-radius: 40px;
+
+          &:focus {
+            outline: none;
+          }
         }
 
-        h2 {
-            text-align: center;
-            font-size: 32px;
-            color: #303030;
-            margin-bottom: 40px;
-
-            @media (min-width: 900px) {
-                font-size: 40px;
-            }
+        .icon {
+          width: 12px;
+          position: absolute;
+          left: 15px;
         }
-
-        .inputs {
-            width: 100%;
-            max-width: 350px;
-
-            .input {
-                position: relative;
-                display:flex;
-                justify-content: center;
-                align-items: center;
-                margin-bottom: 8px;
-
-                input {
-                    width: 100%;
-                    border: none;
-                    background-color: #f2f7f6;
-                    padding: 4px 4px 4px 40px;
-                    height: 50px;
-                    border-radius: 40px;
-
-                    &:focus {
-                        outline: none;
-                    }
-                }
-
-                .icon {
-                    width: 12px;
-                    position: absolute;
-                    left: 15px;
-                }
-            }
-        }
-
-        .forgot-password {
-            text-decoration: none;
-            color: #000;
-            cursor: pointer;
-            font-size: 14px;
-            margin: 16px 0 32px;
-            border-bottom: 1px solid transparent;
-            transition: .5s ease all;
-
-            &:hover {
-                border-color: #303030;
-            }
-        }
-
-        .angle {
-            display: none;
-            position: absolute;
-            background-color: #fff;
-            transform: rotateZ(3deg);
-            width: 60px;
-            right: -30px;
-            height: 101%;
-
-            @media (min-width: 900px) {
-                display: initial;
-            }
-        }
+      }
     }
 
-    .background {
-        display: none;
-        flex: 2;
-        background-size: cover;
-        background-image: url('../assets/background.png');
-        width: 100%;
-        height: 100%;
+    .forgot-password {
+      text-decoration: none;
+      color: #000;
+      cursor: pointer;
+      font-size: 14px;
+      margin: 16px 0 32px;
+      border-bottom: 1px solid transparent;
+      transition: 0.5s ease all;
 
-        @media (min-width: 900px) {
-            display: initial;
-        }
+      &:hover {
+        border-color: #303030;
+      }
     }
+
+    .angle {
+      display: none;
+      position: absolute;
+      background-color: #fff;
+      transform: rotateZ(3deg);
+      width: 60px;
+      right: -30px;
+      height: 101%;
+
+      @media (min-width: 900px) {
+        display: initial;
+      }
+    }
+  }
+
+  .background {
+    display: none;
+    flex: 2;
+    background-size: cover;
+    background-image: url('../assets/background.png');
+    width: 100%;
+    height: 100%;
+
+    @media (min-width: 900px) {
+      display: initial;
+    }
+  }
 }
 </style>
